6e5b3fde9df2d1cc63dada9fe001571e27f38375
M V S M Satvika <87240425+Satvika1806@users.noreply.github.com>
2021-08-09 14:37:38 +0530

    updated the location of Client UI

--- src/main/java/app/Client.java
+++ src/main/java/app/Client.java
@@ -31,8 +31,8 @@ public class Client extends JFrame{
         getContentPane().setBackground(Color.BLACK);        
         setSize(700, 550);   
         setResizable(false);
-        setLocation(760,200);    //to appear this CLIENT frame at a location of 760,200 on our system
-        setUndecorated(true);   
+        setLocation(600,100);    //to appear this CLIENT frame at a location of 600,100 on our system
+        setUndecorated(true);      //minimise and drag options to be available to user   
         setLayout(null); 
         
         p1 = new JPanel();     
